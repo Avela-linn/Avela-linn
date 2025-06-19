0a0caf29c249e185fdbcb977fe2833e5c1bcf523
Avela-linn <avelampikeleli9@gmail.com>
2025-06-19 09:21:48 +0200

    Tableau  Project SQL Queries.sql

--- Tableau Portfolio Project SQL Queries.sql
+++ Tableau Portfolio Project SQL Queries.sql
@@ -1,10 +1,3 @@
-/*
-
-Queries used for Tableau Project
-
-*/
-
-
 
 -- 1. 
 
@@ -25,12 +18,6 @@ order by 1,2
 --where location = 'World'
 ----Group By date
 --order by 1,2
-
-
--- 2. 
-
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
@@ -99,8 +86,6 @@ where continent is not null
 order by 1,2
 
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
 
 
 --Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
